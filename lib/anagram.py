103de92242145d69eb08cd56a97cb6c3746b0df4
KELVIN <kevrith@gmail.com>
2025-11-18 14:03:27 +0300

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        target_letters = sorted(self.word)
+        matches = []
+        for word in word_list:
+            if sorted(word.lower()) == target_letters and word.lower() != self.word:
+                matches.append(word)
+        return matches
